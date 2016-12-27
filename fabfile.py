85db10d9a9c9c04d1e20508c8850d75b88229779
Igor Barsi <ibarsi@unioncreative.com>
2016-12-27 13:43:11 -0500

    - Added deploy script and project settings file example. - Added Python virtualenv with Fabric (used for deploy over SSH).

---
+++ fabfile.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys
+import json
+
+from fabric.api import env, task, cd, run, prompt
+
+from settings import HOST, CODE_DIR, PM2_APP_NAME
+
+# CONFIG
+env.hosts = [HOST]
+env.use_ssh_config = True
+
+
+def get_version():
+    with open('package.json') as package_file:
+        package = json.load(package_file)
+
+    return package['version']
+
+
+@task
+def deploy():
+    print 'Running on {0} as {1}'.format(env.hosts, env.user)
+
+    default_tag = get_version()
+
+    tag = prompt('Please enter {0} {1} [default: {2}]: '.format('tag', '(eg. 1.0.0)', default_tag))
+    tag = default_tag if tag in [None, ''] else tag
+
+    with cd(CODE_DIR):
+        run('git checkout tags/{0}'.format(tag))
+        run('npm run build')
+        run('npm prune')
+        run('npm install')
+        run('sudo pm2 reload {0}'.format(PM2_APP_NAME))
---
+++ settings.py.ex
@@ -0,0 +1,3 @@
+HOST = 'host-name'
+CODE_DIR = '~/path/to/remote/code/dir`'
+PM2_APP_NAME = 'pm2-app-name'
